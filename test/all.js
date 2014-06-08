768dcf6594097eb40e52865b37269de55c0c9025
Jason Frame <jason@onehackoranother.com>
2014-06-08 02:18:00 +0100

    tests

--- test/all.js
+++ test/all.js
@@ -1,1 +1,57 @@
 var test = require('tape');
+var stateObject = require('..');
+
+test('get state', function(assert) {
+
+    var so = stateObject('foo');
+    assert.ok(so.get() === 'foo');
+    assert.end();
+
+});
+
+test('set state', function(assert) {
+
+    var so = stateObject('bar');
+    so.set('bleem');
+    assert.ok(so.get() === 'bleem');
+    assert.end();
+
+});
+
+test('guardedTimeout calls callback when state is unchanged', function(assert) {
+
+    assert.plan(1);
+
+    var so = stateObject('baz');
+
+    var x = 0;
+
+    so.guardedTimeout(function() {
+        x = 1;
+    }, 100);
+
+    setTimeout(function() {
+        assert.equal(x, 1);
+    }, 200);
+
+});
+
+test('guardedTimeout does not cal callback is state is changed', function(assert) {
+
+    assert.plan(1);
+
+    var so = stateObject('baz');
+
+    var x = 0;
+
+    so.guardedTimeout(function() {
+        x = 1;
+    }, 100);
+
+    setTimeout(function() {
+        assert.equal(x, 0);
+    }, 200);
+
+    so.set('raaaa');
+
+});
